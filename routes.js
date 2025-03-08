75311d72d7de133a1b485b57a4a7ace508ae1211
Saralla-Rohit <rohitsarla69@gmail.com>
2025-03-08 12:54:33 +0530

    added contactus with validations

--- routes.js
+++ routes.js
@@ -3,7 +3,8 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import auth_routes from "./routes/auth_routes.js";
 import category_routes from "./routes/category.routes.js";
-import product_routes from './routes/product_routes.js'
+import product_routes from './routes/product_routes.js';
+import query_routes from "./routes/query_routes.js"
 const app = express();
 
 app.use(express.json());
@@ -17,5 +18,7 @@ app.use("/api/v1/auth", auth_routes);
 app.use("/api/v1/category", category_routes);
 
 app.use("/api/v1/product", product_routes);
+
+app.use("/api/v1/contact-us", query_routes);
 
 export default app;
