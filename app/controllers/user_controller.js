305ab1649e2527e5e467db7c342acc36e0ad0773
chandupriya <priyadavuluru@gmail.com>
2025-03-11 18:18:57 +0530

    changes in user updates

--- app/controllers/user_controller.js
+++ app/controllers/user_controller.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import { User } from "../models/user.model.js";
+import { Product } from "../models/product.model.js";
 
 export const profileUpdate = async (req, res) => {
     const { phone_number, fullName, avatar, dateOfBirth } = req.body;
@@ -34,12 +35,31 @@ export const getUserProfile = async (req, res) => {
 
 export const getUsers = async (req, res) => {
     try {
-        const users = await User.find({ role: 'user' }).select("-password -refreshToken -otp");
-        res.status(200).json(users);
+        const usersWithProductCount = await User.aggregate([
+            { $match: { role: 'user' } },
+            {
+                $lookup: {
+                    from: 'products',
+                    localField: '_id',
+                    foreignField: 'user',
+                    as: 'products'
+                }
+            },
+            {
+                $project: {
+                    name: 1,
+                    email: 1,
+                    status: 1,
+                    product_count: { $size: '$products' }
+                }
+            }
+        ]);
+        res.status(200).json({ users: usersWithProductCount });
     } catch (err) {
         return res.status(500).json({ message: 'Something went wrong... please try again later' });
     }
-}
+};
+
 
 export const updateUser = async (req, res) => {
     try {
@@ -49,10 +69,11 @@ export const updateUser = async (req, res) => {
             return res.status(400).json({ message: "Invalid user ID" });
         }
         const user = await User.findByIdAndUpdate(id, { $set: { status } }, { new: true });
+        const products = await Product.updateMany({ user: id }, { $set: { status } }, { new: true });
         if (!user) {
             return res.status(404).json({ message: "User not found" });
         }
-        res.status(200).json({ user, message: "User status successfully" })
+        res.status(200).json({ user, products, message: "User status successfully" })
     } catch (err) {
         return res.status(500).json({ message: 'Something went wrong... please try again later' });
     }
