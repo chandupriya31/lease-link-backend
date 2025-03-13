9aea5c9cbd55c3614e8453fdea8b5d73b1b0ed01
chandupriya <priyadavuluru@gmail.com>
2025-03-13 16:37:25 +0530

    blog route changes

--- routes/blog_routes.js
+++ routes/blog_routes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import multer  from 'multer'
+import multer from 'multer'
 import { getAllBlogs, getBlogById, createBlog, editBlog, deleteBlog } from "../app/controllers/blog_controller.js"
 import blogValidator from "../app/validators/blog_validation.js"
 import { authenticateUser, isAuthorizedUser } from '../app/middlewares/auth_middlewares.js';
@@ -7,9 +7,9 @@ import { checkSchema } from "express-validator";
 const upload = multer()
 
 const router = express.Router()
-router.post("/add-blog", authenticateUser, isAuthorizedUser,upload.single('image'), checkSchema(blogValidator), createBlog)
+router.post("/add-blog", authenticateUser, isAuthorizedUser, upload.single('image'), checkSchema(blogValidator), createBlog)
 router.get("/:id", getBlogById)
-router.get("/blogs", getAllBlogs)
+router.get("", getAllBlogs)
 router.put("/:id", authenticateUser, isAuthorizedUser, checkSchema(blogValidator), editBlog)
 router.delete("/:id", deleteBlog)
 export default router
