900a9a0ad4d23d97f098cc709f0aa5f9307f4fbd
Srinu Vemula <vemulasrinuiso@gmail.com>
2025-03-13 20:42:50 +0530

    update small changes

--- app/controllers/blog_controller.js
+++ app/controllers/blog_controller.js
@@ -69,14 +69,30 @@ export const createBlog = async (req, res) => {
         return res.status(500).send({ message: "Something Went Wrong! Please try again later", err })
     }
 }
+
 export const editBlog = async (req, res) => {
+    console.log("update blog")
     const errors = validationResult(req);
+    
     if (!errors.isEmpty()) {
         return res.status(400).json({ error: errors.array() })
     }
     try {
+        
         const id = req.params.id
         const updates = req.body
+
+        const existingBlog = await Blog.findById(id);
+                if (!existingBlog) {
+                    return res.status(404).json({ message: "Blog not found with that ID" });
+                }
+        
+                // Handle file upload if a new image is provided
+                if (req.file) {
+                    const imageUrl = await uploadToS3(req.file); // Upload image to S3
+                    updates.image = imageUrl; // Add image URL to updates
+                }
+
         const updatedBlog = await Blog.findByIdAndUpdate(id, updates, { new: true })
         if (!updatedBlog) {
             return res.status(404).json({
@@ -93,6 +109,8 @@ export const editBlog = async (req, res) => {
         })
     }
 }
+
+
 export const deleteBlog = async (req, res) => {
     try {
         const id = req.params.id;
